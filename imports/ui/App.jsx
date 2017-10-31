c3f3fa130b7ab88c443dbc6a06b1af6976e00c45
Julio Poveda <ja.poveda10@uniandes.edu.co>
2017-10-31 07:58:07 -0500

    Se quitaron unas lineas de código comentadas
    También se quitó un console.log. Me parece que el código está muy bien organizado, es chévere lo que pusieron dentro de <pre></pre>

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -24,10 +24,6 @@ class App extends Component {
     }
 
     handleVote(evt) {
-      // Games.update(this.state.activeGameId, {
-      //   $inc: { sum: parseInt(evt.target.value), votes: 1 },
-      // });
-
       Meteor.call('games.addScore', this.state.activeGameId, parseInt(evt.target.value));
     }
 
@@ -43,8 +39,6 @@ class App extends Component {
 
     submit(game) {
       game.creator = this.props.currentUser.username;
-      console.log(game);
-      // var id = Games.insert(game);
       Meteor.call('games.insert', game);
       this.setState({ creating: false,
       activeGameId: undefined });
